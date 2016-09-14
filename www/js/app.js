2d4c91ea43f36c1d9a0931429fe18d73c8b40472
vadim semenko <vadimsemenko@gmail.com>
2016-09-14 18:38:36 +0300

    start app

---
+++ www/js/app.js
@@ -0,0 +1,67 @@
+var app = angular.module('myApp', [
+    'ionic',
+    'controllers',
+    'ngStorage'
+]);
+
+app.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+});
+
+app.config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+
+        .state('app', {
+            url: '/app',
+            abstract: true,
+            templateUrl: 'templates/menu.html',
+            controller: 'AppCtrl'
+        })
+
+        .state('app.search', {
+            url: '/search',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/search.html'
+                }
+            }
+        })
+
+        .state('app.browse', {
+            url: '/browse',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/browse.html'
+                }
+            }
+        })
+        .state('app.playlists', {
+            url: '/playlists',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlists.html',
+                    controller: 'PlaylistsCtrl'
+                }
+            }
+        })
+
+        .state('app.single', {
+            url: '/playlists/:playlistId',
+            views: {
+                'menuContent': {
+                    templateUrl: 'templates/playlist.html',
+                    controller: 'PlaylistCtrl'
+                }
+            }
+        });
+
+    $urlRouterProvider.otherwise('/app/playlists');
+});
